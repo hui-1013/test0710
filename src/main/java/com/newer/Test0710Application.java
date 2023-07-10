daf638bc57e94a5b2603a47663ea9fb6fb1c54ff
hui-1013 <2698655426@qq.com>
2023-07-10 18:57:30 +0800

    Initial commit

---
+++ src/main/java/com/newer/Test0710Application.java
@@ -0,0 +1,13 @@
+package com.newer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0710Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0710Application.class, args);
+    }
+
+}
